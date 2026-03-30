ae616ff3bf2fa10b2f19b929bd3e559261a677f3
LironeFitoussi <lironefit@icloud.com>
2026-03-30 11:57:39 +0300

    [FEAT] added ATM Fianl status working

---
+++ python_lab/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+def deposite():
+#     if auth(user_account["pin"]):
+#         amount = float(input("Please type the amount you would like to deposite: "))
+#         if amount < 0:
+#             print("You cannot tpye negative numbers")
+#             exit()
+#         else:
+#             user_account["balance"] += amount
+#             balance()
+#     else:
+#         print("Wrong PIN, goodbye")
+#         exit()
